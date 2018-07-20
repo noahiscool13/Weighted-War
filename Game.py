6e85b455c9b2bff12dc3866c371bbf337a6955ef
noahiscool13 <noah@gamestoplay.nl>
2018-07-21 00:07:44 +0200

    Add can_play function

--- Game.py
+++ Game.py
@@ -3,3 +3,9 @@ class Game:
         p_1_cards = list(range(1, 11))
         p_2_cards = list(range(1, 11))
         middle_cards = list(range(1, 11))
+        p_1_score = p_2_score = 0
+
+    def can_play(self,player,card):
+        if player == 1:
+            return card in self.p_1_cards
+        return card in self.p_2_cards
