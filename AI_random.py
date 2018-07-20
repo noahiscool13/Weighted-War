6ae05fcd054e61ad76c91a08fcbd13799b2d959e
jonay2000 <jonabent@gmail.com>
2018-07-21 01:42:31 +0200

    ??

--- AI_random.py
+++ AI_random.py
@@ -5,7 +5,7 @@ from Game import *
 class AI(Player):
 
     def move(self, current, player):
-        game = self.current_game
+        game = self.current_game()
         if player == 1:
             return choice(game.p_1_cards)
         return choice(game.p_2_cards)
--- test.py
+++ test.py
@@ -4,11 +4,12 @@ from Game import *
 from AI_random import AI as AI1
 from AI_mirror import AI as AI2
 
-t = Tournament()
 
-p1 = AI(t)
-p2 = AI(t)
+p1 = AI1()
+p2 = AI2()
 
-t.new_game(p1,p2)
+t = Tournament(p1,p2)
 
-t.play()
+
+
+print(t.playngames(2))
