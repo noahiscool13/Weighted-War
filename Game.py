83ef6a1397cee3e25b5f91e2385b91528715c726
jonay2000 <jonabent@gmail.com>
2018-07-21 00:31:51 +0200

    Game class should work

--- Game.py
+++ Game.py
@@ -2,8 +2,6 @@ import random
 
 class Game:
 
-
-
   p_1_cards = list(range(1, 11))
   p_2_cards = list(range(1, 11))
   middle_cards = list(range(1, 11))
@@ -13,17 +11,49 @@ class Game:
 
   current = None
 
-  def __init__(self,p1:player,p2:player):
+  winner = None
+  terminated = False
+
+  def __init__(self,p1,p2):
     self.p1 = p1
     self.p2 = p2
 
 
-  def newcard(self):
-    self.current = random.choice(middle_cards)
-    self.middle_cards.remove(self.current)
+  def turn(self,again=[]):
+    current = random.choice(self.middle_cards)
+    middle_cards.remove(current)
 
-  def playcard(self,cardnum:int)
-    pass
+    try:
+      self.newcard()
+    except IndexError:
+      return self.end_game()
+
+    p1card = self.p1.play(current,1)
+    p2card = self.p2.play(current,2)
+
+    self.p_1_cards.remove(p1card)
+    self.p_2_cards.remove(p2card)
+
+    if self.can_play(1,p1card): 
+      if self.can_play(2,p2card):
+        if p1card > p2card:
+          p1score += current + sum(again)
+        elif p1card == p2card:
+          return self.turn([current] + again)
+        else:
+          p2score += current + sum(again)
+      else:
+        self.p2score = -1
+        self.end_game()
+    else:
+      self.p1score = -1
+      self.end_game()
+
+  def end_game(self):
+    self.winner = 1 if self.p1score > self.p1score else 2
+    self.winner = 0 if self.p1score == self.p2score else self.winner
+
+    self.terminated = True
 
   def can_play(self,player,cardnum):
     if player == 1:
@@ -31,8 +61,5 @@ class Game:
     return cardnum in self.p_2_cards
 
 
-class player:
-  def play(self):
-    pass
 
 
